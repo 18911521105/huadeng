fffaeb582505b2551737d1419e54c922cd563134
huadeng <hua_deng@sina.com>
2017-12-04 11:59:29 +0800

    创建项目

---
+++ dysms/src/main/java/com/alicom/dysms/api/SmsUtils.java
@@ -0,0 +1,66 @@
+package com.alicom.dysms.api;
+
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+
+public class SmsUtils {
+
+    //产品名称:云通信短信API产品,开发者无需替换
+    static final String product = "Dysmsapi";
+    //产品域名,开发者无需替换
+    static final String domain = "dysmsapi.aliyuncs.com";
+
+    // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
+    static final String accessKeyId = "LTAIA7L5fBP11vCN";
+    static final String accessKeySecret = "wd7OOkL5ZHY7vssL0utLka0nCjC2Ud";
+
+    public static void main(String[] args) {
+		System.out.println(sendSms("18911521105", "{\"code\":\"123\"}", "卷柏健康", "SMS_109440257"));
+	}
+    
+    public static boolean sendSms(String mobile, String templateParam, String signName, String templateCode) {
+    	boolean result = false;
+        try {
+			//可自助调整超时时间
+			System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
+			System.setProperty("sun.net.client.defaultReadTimeout", "10000");
+
+			//初始化acsClient,暂不支持region化
+			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+			IAcsClient acsClient = new DefaultAcsClient(profile);
+
+			//组装请求对象-具体描述见控制台-文档部分内容
+			SendSmsRequest request = new SendSmsRequest();
+			//必填:待发送手机号
+			request.setPhoneNumbers(mobile);
+			//必填:短信签名-可在短信控制台中找到
+			request.setSignName(signName);
+			//必填:短信模板-可在短信控制台中找到
+			request.setTemplateCode(templateCode);
+			//可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+			request.setTemplateParam(templateParam);
+
+			//选填-上行短信扩展码(无特殊需求用户请忽略此字段)
+			//request.setSmsUpExtendCode("90997");
+
+			//可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+			//request.setOutId("yourOutId");
+
+			//hint 此处可能会抛出异常，注意catch
+			SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+			if(null != sendSmsResponse && "OK".equals(sendSmsResponse.getCode())) {
+				result = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+        return result;
+    }
+}
